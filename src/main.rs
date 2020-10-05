04644fa7bcfb14577663e081cbeade1355c21498
sts10 <sschlinkert@gmail.com>
2020-10-04 22:15:29 -0400

    stuck, but wrote a good test I think?

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,22 @@ fn main() {
     // https://github.com/colmmacc/s2n/blob/7ad9240c8b9ade0cc3a403a732ba9f1289934abd/utils/s2n_random.c#L187
     //  https://lemire.me/blog/2019/06/06/nearly-divisionless-random-integer-generation-on-various-systems/
     //  https://arxiv.org/pdf/1805.10941.pdf
+    //
+    //  Here's where I'm stuck at this point:
+    //  https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=a2533f5f5e64e451d9c8910e4ce048a9
 
     // learning_about_simplest_dice_roll();
     // rejection_fix();
     // lemire_unfair();
     // println!("Lemiere slow give: {}", lemire_slow());
-    println!("Lemiere slow give: {}", lemire_slow_test2(39));
+    // let seed_to_test = 39;
+    for seed_to_test in 0..=255 {
+        println!(
+            "Lemiere slow for {} gives: {:?}",
+            seed_to_test,
+            lemire_slow_test(seed_to_test)
+        );
+    }
 }
 
 fn roll(seed: u8) -> u8 {
@@ -125,20 +135,77 @@ fn lemire_slow() -> usize {
     }
 }
 
-fn lemire_slow_test(seed: u8) -> usize {
-    loop {
-        // let seed = rand::random::<u8>(); // get a random number from 0..=255
-        let m: usize = seed as usize * 6; // Note that the maximum value of m is 255 * 6 or 1,530
-        let l = m % 8;
-        if l < (8 % 6) {
-            return m >> 8;
-        }
+fn lemire_slow_test(seed: u8) -> Option<usize> {
+    // loop {
+    // let seed = rand::random::<u8>(); // get a random number from 0..=255
+    let m: usize = seed as usize * 6; // Note that the maximum value of m is 255 * 6 or 1,530
+    let l = m % 256;
+    println!("m is {}; l in {}", m, l);
+    if l > (256 % 6) {
+        return Some(m >> 8);
+    } else {
+        None
     }
-    // In my testing I found a bad distribution:
-    // Seeds of 0 to 40 (41 seeds) gives you 0
-    // 44 to 84 (41 seeds) gives you 1
-    // 88 to 124 (37 seeds) gives you 2
-    // 128 to 168 (41 seeds) gives you 3
-    // 172 to 212 (41 seeds) gives you 4
-    // 216 to 244 (29 seeds) gives you 5
+    // Seeds 1 to 42    give dice 0 (42)
+    // Seeds 44 to 85   give dice 1 (42)
+    // Seeds 87 to 127  give dice 2 (41)
+    // Seeds 129 to 170 give dice 3 (42)
+    // Seeds 172 to 213 give dice 4 (42)
+    // Seeds 215 to 255 give dice 5 (41)
+}
+
+// fn lemire_medium_test(seed: u8) -> usize {
+//     let m: usize = seed as usize * 6; // Note that the maximum value of m is 255 * 6 or 1,530
+//     let l = m % 8;
+//     if l < 6 {
+//         let floor = 8 % 6;
+
+//         return m >> 8;
+//     }
+
+// }
+//
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    fn make_distribution() -> HashMap<usize, usize> {
+        let mut all_results: Vec<usize> = vec![];
+        let lower = 0;
+        let upper = 255;
+        for this_seed in lower..=upper {
+            match lemire_slow_test(this_seed) {
+                Some(result) => all_results.push(result),
+                None => continue,
+            }
+        }
+
+        let mut counts_hashmap: HashMap<usize, usize> = HashMap::new();
+        for result in all_results {
+            counts_hashmap
+                .entry(result)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        }
+        counts_hashmap
+    }
+
+    fn is_distribution_perfectly_even(counts_hashmap: HashMap<usize, usize>) -> bool {
+        let count_vec: Vec<(&usize, &usize)> = counts_hashmap.iter().collect();
+        let first_count = count_vec[0].1;
+        for result in &count_vec {
+            if result.1 != first_count {
+                println!("Returning false\n{:?}", count_vec);
+                return false;
+            }
+        }
+        true
+    }
+
+    #[test]
+    fn even_distribution() {
+        assert!(is_distribution_perfectly_even(make_distribution()));
+    }
 }
