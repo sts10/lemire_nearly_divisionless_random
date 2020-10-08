7e3b2d42be2fdf077c338c8d62281c6e397c5f18
sts10 <sschlinkert@gmail.com>
2020-10-07 20:08:34 -0400

    makes some nice changes to the readable version

--- src/lib.rs
+++ src/lib.rs
@@ -30,10 +30,12 @@ pub fn roll_using_readable_lemire(s: u8) -> u16 {
     loop {
         let seed = rand::random::<u8>(); // get a random number from 0..=255
         match lemire_from_seed(seed, s) {
-            // if we get a result, that means we got a good seed and thus we got a roll
-            Some(r) => return r,
-            // got a bad seed and thus no roll.
-            // try loop again
+            // if we get a an m value back, that means we had a seed that produced a "good" m
+            // meaning an m we can use to generate a roll result
+            Some(m) => return convert_an_m_to_a_roll_result(m),
+            // If we're here, we got a bad seed and thus a bad m. No roll result
+            // returned by lemire_from_seed function.
+            // So let's go back to the top of the `loop`.
             None => continue,
         };
     }
@@ -43,15 +45,13 @@ fn lemire_from_seed(seed: u8, s: u8) -> Option<u16> {
     let m: u16 = seed as u16 * s as u16; // maximum value of m is 255 * s (if s == 6, then max of m is 1,530)
     let l: u8 = modulo_256(m); // this is a faster alternative to let l = m % 256 (see: https://doc.rust-lang.org/rust-by-example/types/cast.html)
     if l >= s {
-        let roll_result = divide_by_256(m);
-        return Some(roll_result);
+        return Some(m);
     }
     let floor: u8 = eight_modulo(s);
     if l < floor {
         return None;
     } else {
-        let roll_result = divide_by_256(m);
-        return Some(roll_result);
+        return Some(m);
     }
 }
 
@@ -67,7 +67,9 @@ fn eight_modulo(s: u8) -> u8 {
 }
 
 // https://github.com/colmmacc/s2n/blob/7ad9240c8b9ade0cc3a403a732ba9f1289934abd/utils/s2n_random.c#L291-L311
-fn divide_by_256(m: u16) -> u16 {
+// This is the same as dividing by 256, which, given our constants, is how we convert an m to a
+// result
+fn convert_an_m_to_a_roll_result(m: u16) -> u16 {
     m >> 8
 }
 
@@ -82,7 +84,10 @@ mod tests {
         let upper = 255;
         for this_seed in lower..=upper {
             match lemire_from_seed(this_seed, 6) {
-                Some(result) => all_results.push(result as usize),
+                Some(m) => {
+                    let roll_result = convert_an_m_to_a_roll_result(m) as usize;
+                    all_results.push(roll_result);
+                }
                 None => continue,
             }
         }
@@ -97,21 +102,32 @@ mod tests {
         counts_hashmap
     }
 
-    fn is_distribution_perfectly_even(counts_hashmap: HashMap<usize, usize>) -> bool {
-        let count_vec: Vec<(&usize, &usize)> = counts_hashmap.iter().collect();
+    fn makes_counts_vector(counts_hashmap: HashMap<usize, usize>) -> Vec<(usize, usize)> {
+        counts_hashmap.into_iter().collect()
+    }
+
+    fn is_distribution_perfectly_even(count_vec: Vec<(usize, usize)>) -> bool {
         let first_count = count_vec[0].1;
         for result in &count_vec {
             if result.1 != first_count {
-                println!("Returning false\n{:?}", count_vec);
                 return false;
             }
         }
-        println!("Returning true\n{:?}", count_vec);
         true
     }
 
     #[test]
+    fn has_exactly_6_different_roll_results() {
+        let distribution = make_distribution();
+        let counts_vec = makes_counts_vector(distribution);
+        assert_eq!(counts_vec.len(), 6);
+    }
+
+    #[test]
     fn even_distribution() {
-        assert!(is_distribution_perfectly_even(make_distribution()));
+        let distribution = make_distribution();
+        println!("Distribution is {:?}", distribution);
+        let counts_vec = makes_counts_vector(distribution);
+        assert!(is_distribution_perfectly_even(counts_vec));
     }
 }
